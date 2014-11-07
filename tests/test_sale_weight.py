0ff1550edcdb57f421088b4de71920887a5785a2
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:42:54 +0100

    Cleanup tests

--- tests/test_sale_weight.py
+++ tests/test_sale_weight.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class SaleWeightTestCase(unittest.TestCase):
-    '''
-    Test Sale Weight module.
-    '''
+    'Test Sale Weight module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_weight')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_weight')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
