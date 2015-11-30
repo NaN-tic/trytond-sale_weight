10e778d2cd3651345ac4ef467b21ec61b49d117f
resteve <resteve@zikzakmedia.com>
2015-12-01 00:24:58 +0100

    Tests

--- tests/test_sale_weight.py
+++ tests/test_sale_weight.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_weight module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleWeightTestCase(unittest.TestCase):
+class SaleWeightTestCase(ModuleTestCase):
     'Test Sale Weight module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_weight')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_weight')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_weight'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleWeightTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
